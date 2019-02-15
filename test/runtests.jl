e033f2f03759daeba2f5469d5701d1fc409abc5d
Lóránt Meszlényi <meszlenyilorant@gmail.com>
2019-02-15 11:43:28 +0100

    Creates some test for 2d shapes.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,11 @@
 using Openscad
 using Test
 
+@testset "2d shapes" begin
+    @test (toString(Circle(10, false)) == "circle(r = 10, center = false);")
+    @test (toString(Square([1, 1], true)) == "square(size = [1, 1], center = true);")
+end
+
 function test2d()
     square = Square([4, 5], true)
     square2 = Square([0, 0], true)
@@ -9,20 +14,19 @@ function test2d()
         Circle(3, false),
         square,
         Translate([1, 1, 1], [
-            Unify([
+            Openscad.Unify([
                 square,
                 square2
             ])
         ]),
         End()
     ])
-    editor(tree, false)
+    tree
 end
 
-function circle()
-    toString(Circle(10, true))
+@testset "complex 2d shape" begin
+    @test (toString(test2d()) == "union(){circle(r = 3, center = false);square(size = [4, 5], center = true);translate([1, 1, 1]) {union(){square(size = [4, 5], center = true);square(size = [0, 0], center = true);};};};")
+end
 
-@testset "circle" begin
-           @test (typeof(test2d()) == String)
-           @test (circle() == "circle(r=10,center=true)")
-       end;
+@testset "3d shapes" begin
+end
